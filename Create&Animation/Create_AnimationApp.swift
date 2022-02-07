80493e58647ab3f653ff20b11cc6356c98629885
Alisa Ts <freiheiit92@mail.ru>
2022-02-07 14:09:42 +0300

    Initial Commit

---
+++ Create&Animation/Create_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Create_AnimationApp.swift
+//  Create&Animation
+//
+//  Created by Alisa Ts on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Create_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
